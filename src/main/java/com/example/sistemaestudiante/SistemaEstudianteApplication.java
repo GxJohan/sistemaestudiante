15e849970c0ce11bf7c504e6b7683e5de221e5e3
C27444 <c27444@utp.edu.pe>
2024-09-16 16:17:49 -0500

    v1

---
+++ src/main/java/com/example/sistemaestudiante/SistemaEstudianteApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemaestudiante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEstudianteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaEstudianteApplication.class, args);
+    }
+
+}
