9532cb7c79147dd588e2a6dcec89613d112db5a0
C27444 <c27444@utp.edu.pe>
2024-09-16 16:35:41 -0500

    v1.3

---
+++ src/main/java/com/example/sistemaestudiante/service/StudentService.java
@@ -0,0 +1,14 @@
+package com.example.sistemaestudiante.service;
+
+import com.example.sistemaestudiante.model.Student;
+import com.example.sistemaestudiante.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+
+}
